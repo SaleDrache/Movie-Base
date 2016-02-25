66aa97a86b7df72311d794a43d6b67ef9eec0caf
SaleDrache <astojadinovicsd@gmail.com>
2016-02-25 17:32:08 +0100

    "edit" and "delete" movie

--- src/app/core/services/movie.service.js
+++ src/app/core/services/movie.service.js
@@ -8,12 +8,31 @@
     /** @ngInject */
     function MovieService($http, $rootScope, config) { 
       var vm = this;
+      vm.deleteMovie = deleteMovie;
+      vm.editMovie = editMovie;
       vm.getMovies = getMovies;
+
 
 	    function getMovies() {
   	    return $http.get(config.api.concat('/movies'));
 	    }
 
+      function editMovie(movieData) {
+
+        var id = movieData.id;
+        var data = {
+          title: movieData.title,
+          director: movieData.director,
+          actors: movieData.actors,
+          year: movieData.year
+        }
+
+        return $http.post(config.api.concat('/movie/edit/') + id, data );
+      }
+
+      function deleteMovie(movieId) {
+        return $http.get(config.api.concat('/movie/delete/') + movieId );
+      }
 	
     }
     
--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -9,11 +9,17 @@
   function MainController(MovieService, $rootScope) {
     var vm = this;
 
+    vm.editMovie = editMovie;
+    vm.deleteMovie = deleteMovie;
     vm.fromStringToDate = fromStringToDate;
     vm.isLoading = false;
+    vm.editingModal = false;
+    vm.deletingModal = false;
     vm.movies = [];
     vm.movieSortType = 'id';
     vm.movieSortReverse = false;
+    vm.openDeleteMovieModal = openDeleteMovieModal;
+    vm.openEditMovieModal = openEditMovieModal;
     vm.sortMovie = sortMovie;
     vm.userLoggedIn = $rootScope.userLoggedIn;
     
@@ -22,6 +28,28 @@
 
     function activate() {
       getMovies();
+    }
+
+    function deleteMovie() {
+      $('#movieModal').modal('hide');
+      MovieService.deleteMovie(vm.deletingMovie.id)
+        .then( function(result) {
+          getMovies();
+        });
+    }
+
+    function editMovie() {
+      $('#movieModal').modal('hide');
+      MovieService.editMovie(vm.editingMovie)
+        .then( function(result) {
+          getMovies();
+        });
+    
+    }
+
+    function fromStringToDate (stringDate) {
+      var newDate = new Date(stringDate);
+      return newDate;
     }
 
     function getMovies() {
@@ -35,9 +63,21 @@
         });
     }
 
-    function fromStringToDate (stringDate) {
-      var newDate = new Date(stringDate);
-      return newDate;
+    function openDeleteMovieModal (movie) {
+      vm.editingModal = false;
+      vm.deletingModal = true;
+      vm.deletingMovie = angular.copy(movie);
+      $("#movieModal").modal();
+
+      
+    }
+
+    function openEditMovieModal (movie) {
+      vm.deletingModal = false;
+      vm.editingModal = true;
+      vm.editingMovie = angular.copy(movie);      
+      $("#movieModal").modal();
+      
     }
 
     function sortMovie(sortType, reverse) {
